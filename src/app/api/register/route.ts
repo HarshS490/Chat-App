0f24e70efd32cf98b0381b8106b7f9baf32e7bf4
HarshS490 <142019268+HarshS490@users.noreply.github.com>
2024-06-29 22:19:25 +0530

    adding login and sign up page and logic

---
+++ src/app/api/register/route.ts
@@ -0,0 +1,40 @@
+import bcrypt from "bcrypt";
+
+import prisma from "@/lib/prismadb";
+import { NextResponse } from "next/server";
+import { hash } from "crypto";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+
+    const { email, userName, password } = body;
+
+    if (!email || !userName || !password) {
+      return new NextResponse("Missing info", { status: 400 });
+    }
+
+    // check if the user already exist
+    const user = await prisma.user.findUnique({
+        where:{
+            email:email
+        }
+    });
+    if(user){
+        return new NextResponse("User Already exists",{status:400});
+    }
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const newUser = await prisma.user.create({
+      data: {
+        email,
+        userName: userName,
+        password: hashedPassword,
+      },
+    });
+
+    return NextResponse.json(newUser);
+  } catch (error) {
+    console.log(error,'error occured while registering user');
+    return new NextResponse("Internal Server error",{status:500});
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ const inter = Inter({subsets:['latin']})
 export default function Home() {
   return (
     <>
-      <div className="">
+      <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
         ChatApp
       </div>
     </>
